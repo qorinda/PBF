064e209009ae7556e46806a4036ef6c8bf0e881e
qorinda <56285294+qorinda@users.noreply.github.com>
2021-03-08 16:11:54 +0700

    Tugas 1

---
+++ web-reactku/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h4>
+          QORINDA YULVARISMA - 1841720084
+        </h4>
+        <a
+          className="App-link"
+          href="https://google.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Google
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
